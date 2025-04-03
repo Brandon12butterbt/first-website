ed967e312b280ead0d641c45b8d3e7bae5bb2661
Brandon Butterworth <AppBrightBeats02@dlp.expert>
2025-04-03 12:56:13 -0400

    Setting up env variables for ng build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,18 @@
 export const environment = {
   production: true,
   supabase: {
-    url: '${SUPABASE_URL',
-    anonKey: '${SUPABASE_ANON_KEY}'
+    url: '%%SUPABASE_URL%%',
+    anonKey: '%%SUPABASE_ANON_KEY%%'
   },
   stripe: {
-    publishableKey: '${STRIPE_PUBLISHABLE_KEY}',
-    priceId: '${STRIPE_PRICE_ID}',
-    basicTokenUrl: '${STRIPE_BASIC_TOKEN_URL}',
-    standardTokenUrl: '${STRIPE_STANDARD_TOKEN_URL}',
-    premiumTokenUrl: '${STRIPE_PREMIUM_TOKEN_URL}',
+    publishableKey: '%%STRIPE_PUB_KEY%%',
+    priceId: '%%STRIPE_PRICE_ID%%',
+    basicTokenUrl: '%%STRIPE_BASIC_URL%%',
+    standardTokenUrl: '%%STRIPE_STANDARD_URL%%',
+    premiumTokenUrl: '%%STRIPE_PREMIUM_URL%%',
   },
   flux: {
-    apiKey: '${FLUX_API_KEY}',
-    accountId: '${FLUX_ACCOUNT_ID}'
+    apiKey: '%%FLUX_API_KEY%%',
+    accountId: '%%FLUX_ACCOUNT_ID%%'
   }
 }; 
